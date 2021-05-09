b5c39cc34f32d1c98e9ee492fa66e9ba79b755e4
M4SHR0 <76655329+M4SHR0@users.noreply.github.com>
2021-05-09 19:00:28 +0900

    バージョン管理対象ファイルのコミット

---
+++ app/src/main/java/com/example/m4shr0/roomwordsample/Word.kt
@@ -0,0 +1,8 @@
+package com.example.m4shr0.roomwordsample
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
